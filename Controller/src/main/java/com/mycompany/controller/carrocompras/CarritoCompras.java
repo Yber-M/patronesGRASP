c5c7e9e8c22aae4a4b053e99fa6a3b3c69c31a7b
Yb-M <ybermarlon@gmail.com>
2023-05-31 11:58:01 -0500

    Se agrego el guion

--- Controller/src/main/java/com/mycompany/controller/carrocompras/CarritoCompras.java
+++ Controller/src/main/java/com/mycompany/controller/carrocompras/CarritoCompras.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class CarritoCompras {
     private List<Producto> productos;
-
+    
     public CarritoCompras() {
         productos = new ArrayList<>();
     }
--- Controller/src/main/java/com/mycompany/controller/model/Producto.java
+++ Controller/src/main/java/com/mycompany/controller/model/Producto.java
@@ -16,22 +16,22 @@ public class Producto {
         return id_producto;
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public double getPrecio() {
-        return precio;
-    }
-
     public void setId_producto(int id_producto) {
         this.id_producto = id_producto;
+    }
+    
+    public String getNombre() {
+        return nombre;
     }
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
+    
+    public double getPrecio() {
+        return precio;
+    }
+    
     public void setPrecio(double precio) {
         this.precio = precio;
     }
